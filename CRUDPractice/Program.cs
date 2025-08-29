057c74f55c84704b70dbc885ad89a4173daf9f72
Mahla <Mahla@DESKTOP-F7B9RSJ>
2025-08-29 20:35:51 +0330

    #practice CRUD

--- CRUDPractice/Program.cs
+++ CRUDPractice/Program.cs
@@ -20,87 +20,140 @@ namespace CRUDPractice
                     connection.Open();
                     Console.WriteLine("Connection opened successfully!");
 
-                    Console.WriteLine("please Enter Your name: ");
-                    string name = Console.ReadLine();
 
-
-                    Console.WriteLine("please Enter Your family: ");
-                    string family = Console.ReadLine();
-
-
-
-                    Console.WriteLine("please Enter Your Age: ");
-                    int age = Convert.ToInt32(Console.ReadLine());
-
-
-                    Console.WriteLine("please Enter Your Mark: ");
-                    int mark = Convert.ToInt32(Console.ReadLine());
-
-
-
-                    string Insertquery = "insert into Student(name,family,age,mark) values(@name,@family,@age,@mark)";
-                    string selectQuery = "select * from Student";
-                    string updateQuery = "update student set name=@NewName where name=@oldName";
-                    string deleteQuery = "delete from Student where name=@Deletename";
-
-                    using (SqlCommand cmd = new SqlCommand(deleteQuery, connection))
+                    while (true)
                     {
-                        cmd.Parameters.AddWithValue("@Deletename", "mobina");
-                        int rowAffacted = cmd.ExecuteNonQuery();
-                        Console.WriteLine($"RowAffected is {rowAffacted} rows delete successfully");
-                    }
+                        Console.WriteLine("\n=== Student Menu ===");
+                        Console.WriteLine("1. Insert Student");
+                        Console.WriteLine("2. Show All Students");
+                        Console.WriteLine("3. Update Student Mark");
+                        Console.WriteLine("4. Delete Student");
+                        Console.WriteLine("5. Exit");
+                        Console.Write("Choose an option: ");
 
+                        int choise = Convert.ToInt32(Console.ReadLine());
 
-                    using (SqlCommand cmd = new SqlCommand(updateQuery, connection))
-                    {
-                        cmd.Parameters.AddWithValue("@oldName", "mahdieh");
-                        cmd.Parameters.AddWithValue("@NewName", "Mobina");
-                        int rowAffacted = cmd.ExecuteNonQuery();
-                        Console.WriteLine($"RowAffected is {rowAffacted} rows update successfully");
-                    }
-
-                    using (SqlCommand cmd = new SqlCommand(Insertquery, connection))
-                    {
-                        cmd.Parameters.AddWithValue("@name", name);
-                        cmd.Parameters.AddWithValue("@family", family);
-                        cmd.Parameters.AddWithValue("@age", age);
-                        cmd.Parameters.AddWithValue("@mark", mark);
-                        int rowAffacted = cmd.ExecuteNonQuery();
-                        Console.WriteLine($"RowAffected is {rowAffacted} rows insert successfully");
-                    }
-
-
-
-                    using (SqlCommand cmd2 = new SqlCommand(selectQuery, connection))
-                    {
-                        using (SqlDataReader reader = cmd2.ExecuteReader())
+                        switch (choise)
                         {
+                            case 1:
+                                insert(connection);
+                                break;
+                            case 2:
+                                fetch(connection);
+                                break;
+                            case 3:
+                                update(connection);
+                                break;
+                            case 4:
+                                delete(connection);
+                                break;
 
 
-                            while (reader.Read())
-                            {
-                                string Name = reader["name"].ToString();
-                                string Family = reader["family"].ToString();
-                                int Age = Convert.ToInt32(reader["age"].ToString());
-                                int Mark = Convert.ToInt32(reader["mark"].ToString());
-
-
-                                Console.WriteLine($"name: {Name}, family: {Family}, age: {Age}, mark: {Mark}");
-                            }
                         }
+
                     }
-
-
-
-                    Console.ReadKey();
-
 
                 }
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+
+
+        }
+        static void delete(SqlConnection connection)
+        {
+            string deleteQuery = "delete from Student where name=@Deletename";
+
+            Console.WriteLine("please enter your name");
+            string Deletename = Console.ReadLine();
+
+            using (SqlCommand cmd = new SqlCommand(deleteQuery, connection))
+            {
+                cmd.Parameters.AddWithValue("@Deletename", Deletename);
+                int rowAffacted = cmd.ExecuteNonQuery();
+                Console.WriteLine($"RowAffected is {rowAffacted} rows delete successfully");
+            }
+        }
+
+        static void insert(SqlConnection connection)
+        {
+
+            Console.WriteLine("please Enter Your name: ");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("please Enter Your family: ");
+            string family = Console.ReadLine();
+
+
+
+            Console.WriteLine("please Enter Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("please Enter Your Mark: ");
+            int mark = Convert.ToInt32(Console.ReadLine());
+
+            string Insertquery = "insert into Student(name,family,age,mark) values(@name,@family,@age,@mark)";
+            using (SqlCommand cmd = new SqlCommand(Insertquery, connection))
+            {
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@family", family);
+                cmd.Parameters.AddWithValue("@age", age);
+                cmd.Parameters.AddWithValue("@mark", mark);
+                int rowAffacted = cmd.ExecuteNonQuery();
+                Console.WriteLine($"RowAffected is {rowAffacted} rows insert successfully");
+            }
+
+        }
+        static void update(SqlConnection connection)
+        {
+            Console.WriteLine("please Enter Your name: ");
+            string oldName = Console.ReadLine();
+
+
+            Console.WriteLine("please Enter Your NewName: ");
+            string NewName = Console.ReadLine();
+
+            string updateQuery = "update student set name=@NewName where name=@oldName";
+            using (SqlCommand cmd = new SqlCommand(updateQuery, connection))
+            {
+                cmd.Parameters.AddWithValue("@oldName", oldName);
+                cmd.Parameters.AddWithValue("@NewName", NewName);
+                int rowAffacted = cmd.ExecuteNonQuery();
+                Console.WriteLine($"RowAffected is {rowAffacted} rows update successfully");
+            }
+        }
+
+        static void fetch(SqlConnection connection)
+        {
+            string selectQuery = "select * from Student";
+            using (SqlCommand cmd2 = new SqlCommand(selectQuery, connection))
+            {
+                using (SqlDataReader reader = cmd2.ExecuteReader())
+                {
+
+
+                    while (reader.Read())
+                    {
+                        string Name = reader["name"].ToString();
+                        string Family = reader["family"].ToString();
+                        int Age = Convert.ToInt32(reader["age"].ToString());
+                        int Mark = Convert.ToInt32(reader["mark"].ToString());
+
+
+                        Console.WriteLine($"name: {Name}, family: {Family}, age: {Age}, mark: {Mark}");
+                    }
+                }
+            }
+
+
+
+            Console.ReadKey();
 
 
 
